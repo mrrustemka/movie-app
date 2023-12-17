3e3cd5028a3d466e9bd5d6046ac489310a3e91fb
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2023-12-17 21:24:29 +0400

    create-header-component

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import { React } from "react";
+import { useState } from "react";
+import { tempMovieData } from "../data/movieData";
+
+export default function Header() {
+  const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState(tempMovieData);
+  return (
+    <>
+      <nav className="nav-bar">
+        <div className="logo">
+          <span role="img">🍿</span>
+          <h1>usePopcorn</h1>
+        </div>
+        <input
+          className="search"
+          type="text"
+          placeholder="Search movies..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <p className="num-results">
+          Found <strong>{movies.length}</strong> results
+        </p>
+      </nav>
+    </>
+  );
+}

4e7a3953bf5350d0d72972d34788ee0b73dbb1a9
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2023-12-22 16:01:53 +0400

    create-logo-component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,16 +2,14 @@ import { React } from "react";
 import { useState } from "react";
 import { tempMovieData } from "../data/movieData";
 import Search from "./Search";
+import Logo from "./Logo";
 
 export default function NavBar() {
   const [movies, setMovies] = useState(tempMovieData);
   return (
     <>
       <nav className="nav-bar">
-        <div className="logo">
-          <span role="img">🍿</span>
-          <h1>usePopcorn</h1>
-        </div>
+        <Logo />
         <Search />
         <p className="num-results">
           Found <strong>{movies.length}</strong> results

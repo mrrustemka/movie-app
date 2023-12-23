8a2bfc96bcd2729fe65b14216dd22ef046482f5c
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2023-12-23 17:18:00 +0400

    fix-tab-name

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import { useState } from "react";
 import NavBar from "./components/NavBar";
-// import Summary from "./components/Summary";
-// import Button from "./components/Button";
 import Main from "./components/Main";
 import { tempMovieData } from "./data/movieData";
 import { tempWatchedData } from "./data/watchedData";
 
 export default function App() {
-  // const [query, setQuery] = useState("");
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
   const [isOpen1, setIsOpen1] = useState(true);

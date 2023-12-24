1d43d36258e9940db7796cee003bdf150ecc1c82
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2023-12-24 18:21:13 +0400

    create-star-rating-component

---
+++ src/components/StarRating.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const containerStyle = { display: "flex", alignItems: "center", gap: "16px" };
+
+const starContainerStyle = {
+  display: "flex",
+  gap: "4px",
+};
+
+const textStyle = {
+  lineHeight: "1",
+  margin: "0",
+};
+
+function StarRating({ maxRating = 5 }) {
+  return (
+    <div style={containerStyle}>
+      <div style={starContainerStyle}>
+        {Array.from({ length: maxRating }, (_, i) => (
+          <span>{i + 1}</span>
+        ))}
+      </div>
+      <p style={textStyle}>{maxRating}</p>
+    </div>
+  );
+}
+
+export default StarRating;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+// import App from "./App";
+import StarRating from "../src/components/StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <App />
+    <StarRating maxRating={5} />
+    <StarRating maxRating={10} />
   </>
 );

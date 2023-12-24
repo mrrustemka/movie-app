1145a46903de85f10b03f6d5f3b2b4a1ddb07a27
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2023-12-24 18:43:10 +0400

    create-star-component

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+import Star from "./Star";
 
 const containerStyle = { display: "flex", alignItems: "center", gap: "16px" };
 
 const starContainerStyle = {
   display: "flex",
-  gap: "4px",
 };
 
 const textStyle = {
@@ -13,14 +13,25 @@ const textStyle = {
 };
 
 function StarRating({ maxRating = 5 }) {
+  const [rating, setRating] = useState(0);
+
+  function handleRating(rating) {
+    setRating(rating);
+  }
   return (
     <div style={containerStyle}>
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
-          <span>{i + 1}</span>
+          <span>
+            <Star
+              key={i}
+              onRate={() => handleRating(i + 1)}
+              full={rating >= i + 1}
+            />
+          </span>
         ))}
       </div>
-      <p style={textStyle}>{maxRating}</p>
+      <p style={textStyle}>{rating || ""}</p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-import StarRating from "../src/components/StarRating";
+import "./index.css";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
+    <App />
   </>
 );

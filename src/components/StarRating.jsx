0a7d904c30b367581ed4306a142a7bf55269bb57
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2023-12-26 21:36:45 +0400

    stylize-star

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -7,17 +7,26 @@ const starContainerStyle = {
   display: "flex",
 };
 
-const textStyle = {
-  lineHeight: "1",
-  margin: "0",
-};
-
-function StarRating({ maxRating = 5 }) {
-  const [rating, setRating] = useState(0);
+function StarRating({
+  maxRating = 10,
+  color = "#fcc419",
+  size = 32,
+  defaultRating = 0,
+  onSetRating,
+}) {
+  const [rating, setRating] = useState(defaultRating);
   const [tempRating, setTempRating] = useState(0);
+  const textStyle = {
+    lineHeight: "1",
+    margin: "0",
+    color: color,
+    fontSize: size,
+  };
+  const [movieRating, setMovieRating] = useState(0);
 
   function handleRating(rating) {
     setRating(rating);
+    onSetRating(rating);
   }
   return (
     <div style={containerStyle}>
@@ -30,6 +39,8 @@ function StarRating({ maxRating = 5 }) {
               full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
               onHoverIn={() => setTempRating(i + 1)}
               onHoverOut={() => setTempRating(0)}
+              color={color}
+              size={size}
             />
           </span>
         ))}

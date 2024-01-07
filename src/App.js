ea740b50814a57933cb76b7167bfdbe568f39621
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2024-01-07 17:17:38 +0400

    create-loader-component

--- src/App.js
+++ src/App.js
@@ -4,27 +4,31 @@ import Main from "./components/Main";
 import SearchResult from "./components/SearchResult";
 import Search from "./components/Search";
 import Logo from "./components/Logo";
-import { tempMovieData } from "./data/movieData";
+// import { tempMovieData } from "./data/movieData";
 import Box from "./components/Box";
 import MovieList from "./components/MovieList";
-import { tempWatchedData } from "./data/watchedData";
+// import { tempWatchedData } from "./data/watchedData";
 import Summary from "./components/Summary";
 import WatchList from "./components/WatchList";
+import Loader from "./components/Loader";
 
 const KEY = "68d1440d";
 
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   let query = "interstellar";
 
   useEffect(function () {
     async function fetchMovies() {
+      setIsLoading(true);
       const res = await fetch(
         `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
       );
-      const data = res.json();
+      const data = await res.json();
       setMovies(data.Search);
+      setIsLoading(false);
     }
     fetchMovies();
   }, []);
@@ -37,9 +41,7 @@ export default function App() {
         <SearchResult movies={movies} />
       </NavBar>
       <Main>
-        <Box>
-          <MovieList movies={movies} />
-        </Box>
+        <Box>{isLoading ? <Loader /> : <MovieList movies={movies} />}</Box>
         <Box>
           <Summary watched={watched} />
           <WatchList watched={watched} />

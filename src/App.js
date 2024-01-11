63f333f72ae1e0a096ffead29222c15c6aa186cb
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2024-01-11 20:17:21 +0400

    modify-search-component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,8 @@ import Main from "./components/Main";
 import SearchResult from "./components/SearchResult";
 import Search from "./components/Search";
 import Logo from "./components/Logo";
-// import { tempMovieData } from "./data/movieData";
 import Box from "./components/Box";
 import MovieList from "./components/MovieList";
-// import { tempWatchedData } from "./data/watchedData";
 import Summary from "./components/Summary";
 import WatchList from "./components/WatchList";
 import Loader from "./components/Loader";
@@ -16,18 +14,19 @@ import ErrorMessage from "./components/ErrorMessage";
 const KEY = "68d1440d";
 
 export default function App() {
+  const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
-  let query = "sadsfdjs";
+  let tempQuery = "new york";
 
   useEffect(function () {
     async function fetchMovies() {
       try {
         setIsLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+          `http://www.omdbapi.com/?apikey=${KEY}&s=${tempQuery}`
         );
 
         if (!res.ok) {
@@ -37,7 +36,6 @@ export default function App() {
         if (data.Response === "False") throw new Error("Movie not found");
         setMovies(data.Search);
       } catch (err) {
-        console.log(err.message);
         setError(err.message);
       } finally {
         setIsLoading(false);
@@ -50,7 +48,7 @@ export default function App() {
     <>
       <NavBar>
         <Logo />
-        <Search />
+        <Search query={query} setQuery={setQuery} />
         <SearchResult movies={movies} />
       </NavBar>
       <Main>
